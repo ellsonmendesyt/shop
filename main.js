95989b3bf445ed0a211492b3322e0834c99be001
ellsonmendesyt <elsonmendesoficial@gmail.com>
2021-11-25 20:22:11 -0300

    antes de começar o react

---
+++ main.js
@@ -0,0 +1,22 @@
+const log = console.log;
+
+
+// selecionar todos os elementos que tem a classe brand
+
+// document.querySelector('.brand').style.backgroundColor='red';
+
+// selecionar todos os elementos que tem FA mas nao tem FA-STAR-O
+// let fas =document.querySelectorAll(".fa:not(.fa-star-o):not(.fa-star-half-o)");
+// let fas = document.querySelectorAll(".fa.fa-star-o");
+
+// fas.forEach((fa)=>{
+//     fa.style.border="2px solid magenta";
+// })
+
+
+
+
+
+
+
+
